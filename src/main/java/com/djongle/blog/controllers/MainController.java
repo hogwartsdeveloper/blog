a0e16490859f0dcb8e88a349996029e10bdbc1d3
django <jannuraidynuly@gmail.com>
2021-04-06 12:22:20 +0600

    create new method view blog

--- src/main/java/com/djongle/blog/controllers/MainController.java
+++ src/main/java/com/djongle/blog/controllers/MainController.java
@@ -1,5 +1,8 @@
 package com.djongle.blog.controllers;
 
+import com.djongle.blog.models.Post;
+import com.djongle.blog.repo.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,15 +10,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 
+    @Autowired
+    private PostRepository postRepository;
+
+
     @GetMapping("/")
     public String home(Model model) {
-        model.addAttribute("title", "Главная страница");
+        Iterable<Post> posts = postRepository.findAll();
+        model.addAttribute("posts", posts);
         return "home";
     }
 
     @GetMapping("/about")
     public String about(Model model) {
-        model.addAttribute("title", "Страница про нас");
+        model.addAttribute("title", "О нас");
         return "about";
     }
 
